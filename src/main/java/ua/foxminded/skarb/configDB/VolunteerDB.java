7a666328315fbbb8bf02f722dd7fdd21f39c9f4e
oleh23 <lcbolko23@gmail.com>
2024-01-17 21:08:57 -0500

    I modify: src/main/ua.foxminded.skarb.configDB/VolunteerDB and create src/test/DbTest/VolunteerDbRegistration

--- src/main/java/ua/foxminded/skarb/configDB/VolunteerDB.java
+++ src/main/java/ua/foxminded/skarb/configDB/VolunteerDB.java
@@ -1,39 +1,72 @@
 package ua.foxminded.skarb.configDB;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.sql.*;
+import java.util.Properties;
 
 public class VolunteerDB {
 
-    public static void main(String[] args) throws SQLException, ClassNotFoundException {
+    private static Connection connection;
 
-        String queryCheckInfo = "Select * from users where email = 'betta.kozak@gmail.com'";
+    public static void initializeDbConnection() throws IOException, ClassNotFoundException, SQLException {
+
+        Properties prop = new Properties();
+        InputStream input = VolunteerDB.class.getClassLoader().getResourceAsStream("db.properties");
+
+        // Load the properties file
+        prop.load(input);
+
+        // Retrieve the property values
+        String dbUrl = prop.getProperty("db.url");
+        String dbUser = prop.getProperty("db.user");
+        String dbPassword = prop.getProperty("db.password");
+
+        // Driver name and connection setup
+        Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+        Connection connection = DriverManager.getConnection(dbUrl, dbUser, dbPassword);
+        System.out.println("Connection Established successfully");
+    }
+
+    public static void insertData(String firstName, String lastName, String email, String password, String sex, String role, String emailConfirmed, String createdDate, String updatedDate, String locale) throws SQLException {
+
+        String queryCheckInfo = "Select * from users where email = " + "'" + email + "'";
         String queryInsertInto = "INSERT INTO users\n" +
                 "  (first_name, last_name, email, password, sex, role, status, email_confirmed, owner, created_date, updated_date, locale)\n" +
                 "VALUES\n" +
-                "  ('Betta', 'Kozak', 'betta.kozak@gmail.com', 'TEst@1234', 'MALE', 'ROLE_VOLUNTEER', 'ACTIVE', 'true', 'true', '2024-01-15 02:46:13', '2024-01-15 02:46:19', 'UA');";
-        // Driver name and connection setup
-        Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
-        Connection connection = DriverManager.getConnection("jdbc:sqlserver://185.149.40.46:1433;database=SkarbNgoDB;encrypt=false", "sa", "37Y5Nb8uTo2");
-        System.out.println("Connection Established successfully");
+                "  ('Gretta', 'Dozak', 'gretta.kozak@gmail.com', 'TEst@1234', 'MALE', 'ROLE_VOLUNTEER', 'ACTIVE', 'true', 'true', '2024-01-15 02:46:13', '2024-01-15 02:46:19', 'UA');";
 
-        Statement statement = connection.createStatement();
+        try (PreparedStatement preparedStatement = connection.prepareStatement(queryInsertInto)) {
+            preparedStatement.setString(1, firstName);
+            preparedStatement.setString(2, lastName);
+            preparedStatement.setString(3, email);
+            preparedStatement.setString(4, password);
+            preparedStatement.setString(5, sex);
+            preparedStatement.setString(6, role);
+            preparedStatement.setString(7, emailConfirmed);
+            preparedStatement.setString(8, createdDate);
+            preparedStatement.setString(9, updatedDate);
+            preparedStatement.setString(10, locale);
 
-        // Insert data
-        int rowsInserted = statement.executeUpdate(queryInsertInto);
-        if (rowsInserted > 0) {
-            System.out.println("Volunteer inserted into DB successfully");
+            Statement statement = connection.createStatement();
+
+            // Insert data
+            int rowsInserted = statement.executeUpdate(queryInsertInto);
+            if (rowsInserted > 0) {
+                System.out.println("Volunteer inserted into DB successfully");
+            }
+
+            // Confirm insertion
+            ResultSet resultSet = statement.executeQuery(queryCheckInfo);
+            if (resultSet.next()) {
+                System.out.println("Confirmed: Data found in DB for email betta.kozak@gmail.com");
+            } else {
+                System.out.println("No data found for email betta.kozak@gmail.com");
+            }
+
+            statement.close();
+            connection.close();
+            System.out.println("Connection Closed");
         }
-
-        // Confirm insertion
-        ResultSet resultSet = statement.executeQuery(queryCheckInfo);
-        if (resultSet.next()) {
-            System.out.println("Confirmed: Data found in DB for email betta.kozak@gmail.com");
-        } else {
-            System.out.println("No data found for email betta.kozak@gmail.com");
-        }
-
-        statement.close();
-        connection.close();
-        System.out.println("Connection Closed");
     }
 }
---
+++ src/test/java/ua/foxminded/skarb/tests/DbTest/VolunteerDbRegistration.java
@@ -0,0 +1,23 @@
+package ua.foxminded.skarb.tests.DbTest;
+
+import org.junit.jupiter.api.Test;
+import ua.foxminded.skarb.configDB.VolunteerDB;
+
+public class VolunteerDbRegistration {
+
+    @Test
+    public void Volunteer () {
+
+        String firstName = "Gretta";
+        String lastName = "Dozak";
+        String email = "gretta.kozak@gmail.com";
+        String password = "TEst@1234";
+        String sex = "MALE";
+        String role = "ROLE_VOLUNTEER";
+        String emailConfirmed = "true";
+        String createdDate = "2024-01-15 02:46:13";
+        String updatedDate = "2024-01-15 02:46:19";
+        String locale = "UA";
+
+    }
+}
